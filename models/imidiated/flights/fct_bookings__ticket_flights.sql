b7ecd58f0c80e593ed39d63a1f1cbb800da141fc
lexaa666 <lexusbirulya@gmail.com>
2025-06-15 13:27:52 +0300

    add new homework 2

---
+++ models/imidiated/flights/fct_bookings__ticket_flights.sql
@@ -0,0 +1,12 @@
+{{
+        config(
+          materialized='table'
+        ) 
+}}
+      select
+        ticket_no,
+        flight_id,
+        fare_conditions,
+        amount
+      from {{ source('bookings', 'ticket_flights') }}
+    
